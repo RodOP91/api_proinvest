9ea908a751692b37c5369bfe322d14fa2fe5895e
Rodrigo Ordóñez <rodrigo.op1791@gmail.com>
2019-12-11 14:09:07 -0600

    Pruebas iniciales

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const config = require('../config/default')
 const apiSolicitud = require('./routes/solicitud')
 
-app.use(bodyParser)
+app.use(bodyParser.urlencoded({extended: true}))
 app.use('/solicitud', apiSolicitud)
 
 app.listen(config.PORT, config.BINDIND_IP, function() {
