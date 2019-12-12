5124f4d0ee2bca33ea0234b2485be6072225cc6f
RodrigoOP <rodrigo.op1791@gmail.com>
2019-12-12 00:49:27 -0600

    Rutas adicionales

--- src/dataaccess/model/Inversionista.js
+++ src/dataaccess/model/Inversionista.js
@@ -2,6 +2,7 @@ const mongoose = require('../MongoConnect')
 const Schema = mongoose.Schema
 
 var InversionistaSchema = new Schema({
+    //PROPIEDADES DEL PASO 1
     nombre:{
         required: true,
         type: String
@@ -38,9 +39,53 @@ var InversionistaSchema = new Schema({
         required: true,
         type: String
     },
+    email_isValidado:{
+        required: false,
+        type: Number,
+    },
     telefono_celular:{
         required: true,
         type: String
+    },
+    telefono_isValidado:{
+        required:false,
+        type: Number,
+    },
+    //TERMINAN PROPIEDADES DEL PASO 1
+
+    //PROPIEDADES DEL PASO 2
+    calle:{
+        required:false,
+        type: String,
+    },
+    numero:{
+        required:false,
+        type: String,
+    },
+    numero_interior:{
+        required:false,
+        type: String,
+    },
+    codigo_postal:{
+        required:false,
+        type: String,
+    },
+    colonia:{
+        required:false,
+        type: String,
+    },
+    municipio:{
+        required:false,
+        type: String,
+    },
+    estado:{
+        required:false,
+        type: String,
+    },
+    //TERMINAN PROPIEDADES DEL PASO 2
+    solicitud_terminada:{
+        required: false,
+        type: Number
     },
 })
 
--- src/routes/InversionistaRoutes.js
+++ src/routes/InversionistaRoutes.js
@@ -3,13 +3,60 @@ const router = express.Router();
 const Inversionista = require('../dataaccess/model/Inversionista')
 
 /**
+ * GET todos los Inversionistas
+ * @params null
+ * @returns Array de Jsons con los Inversionistas
+ */
+router.get('/all', (req, res)=>{
+    Inversionista.find((err, docs)=>{
+        if(err){
+            res.status(500).json({
+                message: 'Hubo un error al recuperar a los Inversionistas',
+                error: err
+            })
+            return
+        }
+        res.json(docs)
+    })
+})
+
+/**
+ * GET one by ID, recupera a un Inversionista por su ID
+ * @params id
+ * @returns inversionista que coincida con la id propocionada
+ */
+router.get('/:id', (req, res)=>{
+    if(!req.params.id){
+        res.status(400).json({
+            message:'Parámetro inexistente',
+            error: req.params.id
+        })
+        return
+    }
+
+    Inversionista.findById({_id: req.params.id}, (err, docs)=>{
+        if(err){
+            res.status(500).json({
+                message: 'Error al recuperar al Inversionista solicitado',
+                error: err.body
+            })
+            console.error(err)
+            return
+        }
+        res.status(200).json(docs)
+    })
+})
+
+
+
+/**
  * POST nuevo Inversionista
  * @params nombre, apellido paterno, apellido materno, rfc, fecha nacimiento, grado académico,
  * profesión, empresa, email y teléfono celular. Corresponde al primer paso de alta de datos del
  * Inversionista
  * @returns res = response
  */
-router.post('/', (req, res) =>{
+router.post('/new', (req, res) =>{
     /**
      * Validación que los parámetros de req.body existan, regresa código 400 si faltan
      */
@@ -46,6 +93,32 @@ router.post('/', (req, res) =>{
         }
         res.status(200).json(doc)
     })
+})
+
+/**
+ * PUT registra los datos del Paso 2 del formulario
+ * @params calle, numero, numero_interior, codigo_postal, colonia, municipio, estado
+ */
+router.put('/paso2/:id', (req, res)=>{
+    if(!req.body.calle || !req.body.numero || !req.body.numero_interior || !req.body.codigo_postal
+        || !req.body.colonia || !req.body.municipio || !req.body.estado){
+            res.status(400).json({
+                message : 'Parámetros incompletos y/o inválidos',
+                request: req.body
+            })
+            return
+        }
+
+        Inversionista.findByIdAndUpdate(req.params.id, {$set: req.body}, (err, docs)=>{
+            if(err){
+                res.status(500).json({
+                    message:'Error al actualizar datos de Inversionista',
+                    error: err.body
+                })
+                return
+            }
+            res.status(200).json(docs)
+        })
 })
     
 
